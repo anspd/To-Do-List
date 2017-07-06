1cb9d28003a04b8d0e9425b21dbb44c1ad049f4a
Anshuman Padhi <anshumanpadhi98@gmail.com>
2017-07-06 14:22:39 +0530

    second build

--- server.js
+++ server.js
@@ -2,139 +2,115 @@ var express=require('express')
 var app=express()
 var bodyParser=require('body-parser')
 var MongoClient = require('mongodb').MongoClient
-var assert = require('assert');
-var db
+var ObjectId= require('mongodb').ObjectID
+var url = 'mongodb://anspd:asdf1234qwe@ds030500.mlab.com:30500/tododata';
 
 app.use(bodyParser.urlencoded({extended: true}))
 
-var url = 'mongodb://anspd:asdf1234qwe@ds030500.mlab.com:30500/tododata';
-
+var db
 MongoClient.connect(url, function(err, database) {
-  assert.equal(null, err)
+  if(err) throw err
   db=database
-  console.log("Connected successfully to server")
+  console.log("Connected successfully to database server")
  })
 
-
-  app.listen(3000, function() {
+ app.listen(3000, function() {
    console.log('listening on 3000')
 })
 
-/*
-var insertDocument = function(db,callback) {
-  db.collection('users').insertOne({
-    "email":"admin@todo.list",
-    "pwd":"password"
-  }, function(err, result) {
-    assert.equal(err,null)
-    console.log("done.")
-    callback()
-  })
-}
-
-// Use connect method to connect to the server
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err)
-  console.log("Connected successfully to server")
-  insertDocument(db,function() {
-    db.close()
-  })
- })
-*/
-
-app.get('/*', (req, res) => {
-  res.sendFile(__dirname + '/login.html')
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
 })
-
 
 var objId
 
+app.post('/users/todos', (req,res) => {
+  var query = req.body
+  console.log(query)
+  db.collection('users').findOne(query,function(err,obj) {
+     if(err) throw err
+     if(obj) {
+         console.log(obj)
+         objId = obj._id
+         console.log("Logged in")
+         res.sendFile(__dirname + '/home.html')
+     }
+     else {
+         console.log("Email Address or Password not found")
+         res.redirect('/')
+     }
+  })
+})
+
+app.post('/users', (req,res) => {
+  var query = {email: req.body.email}
+  db.collection('users').findOne(query,function(err,obj) {
+     if(err) throw err
+     if(obj) {
+         console.log("User already exists")
+         res.redirect('/')
+     }
+     else {
+         if(req.body.pwd==req.body.confpwd) {
+             db.collection('users').insert( { email: req.body.email , pwd: req.body.pwd }, function(err, result) {
+                 if(err) throw err
+                 console.log("Login to continue")
+                 res.redirect('/')
+             })
+         }
+         else {
+             console.log("Passwords do not match")
+             res.redirect('/')
+         }
+     }
+  })
+})
 
 var display = function(res){
       db.collection('todos').find({parentId: objId}).toArray(function(err,result){
         if(err) throw err
+        console.log("Sending all the todos in response")
         console.log(result)
         res.send(result)
       })
     }
 
-app.post('/todolist', (req, res) => {
-  var query = req.body
-  var count=db.collection('users').count(query,function(err,result) {
-     if(err) throw err
-     if(result==1)  {
-       db.collection('users').findOne(query,(err,obj) => {
-         objId = obj._id
-       })
-       res.sendFile(__dirname + '/index.html')
-     }
-     else res.redirect('/') 
-  })
-})
-
-var insertDocument = function(db,body, callback) {
-   db.collection('users').insert( { email: body.email , pwd: body.pwd }, function(err, result) {
-    assert.equal(err, null);
-    console.log("Inserted a document into the users collection.");
-    callback();
-  })
-}
-
-app.post('/signup', (req,res) => {
-  console.log(req.body)
-  var query = {email: req.body.email}
-  var count=db.collection('users').count(query, (err,result) => {
-    if(err) throw err
-    if(result == 0)
-     insertDocument(db,req.body, function() {
-       res.redirect('/')
-     });
-     else res.redirect('/')
-    })
-  })
-
-
-app.post('/initialize', (req,res)=> {
+app.get('/todos', (req,res)=> {
   display(res)
 })
 
-
-app.post('/add', (req,res) => {
-  console.log(req.body)
+app.post('/todos', (req,res) => {
   console.log({ parentId: objId, todo: req.body.todo, isCmpltd:"no" })
   db.collection('todos').insert( { parentId: objId, todo: req.body.todo, isCmpltd:"no" }, function(err, result) {
-    assert.equal(err, null);
+    if(err) throw err
     console.log("Inserted a todo into the todos collection.");
     display(res)
   })
 })
 
-app.post('/remove', (req,res) => {
-  db.collection('todos').findOneAndDelete( { parentId: objId, todo: req.body.todo }, function(err, result) {
-    assert.equal(err, null);
-    console.log(result)
+app.delete('/todos/:id', (req,res) => {
+  var query={"parentId": objId,"_id": new ObjectId(req.params.id)}
+  try{
+    db.collection('todos').deleteOne(query)
     console.log("Removed a todo from the todos collection.");
     display(res)
-  })
+  }
+  catch(e) {
+    print(e)
+  }
 })
 
-app.post('/isCmpltdYes', (req,res) => {
-  console.log(req.body)
-  db.collection('todos').findOneAndUpdate( { parentId: objId, todo: req.body.todo }, {$set: {isCmpltd:"yes"}}, (err,result)=> {
+app.put('/todos/:id', (req,res) => {
+    var query={"parentId": objId,"_id": new ObjectId(req.params.id)}
+    console.log(query)
+ db.collection('todos').findOne( query, (err,result)=> {
     if(err) console.log(err)
     console.log(result)
+    if(result.isCmpltd=="yes")
+    db.collection('todos').updateOne({_id: result._id},{$set: {isCmpltd:"no"}})
+    else if(result.isCmpltd=="no")
+    db.collection('todos').updateOne({_id: result._id},{$set: {isCmpltd:"yes"}})
+    console.log("Updated a record")
     res.send()
   })
 })
-
-app.post('/isCmpltdNo', (req,res) => {
-  console.log(req.body)
-  db.collection('todos').findOneAndUpdate( { parentId: objId, todo: req.body.todo }, {$set: {isCmpltd:"no"}}, (err,result)=> {
-    if(err) console.log(err)
-    console.log(result)
-    res.send()
-  })
-})
-
-
-  
